0c3aee2840804b11d3a1623d5fc9f6174aca0cba
Alexander <ermakovich@km.ru>
2022-10-17 20:22:32 +0700

    Hello Random!

--- Examples003_Sum/Program.cs
+++ Examples003_Sum/Program.cs
@@ -1,5 +1,7 @@
 ﻿// третья программа
-int numberA = 4;
-int numberB = 13;
+int numberA = new Random ().Next(1, 10); // 1 2 3 4 ... 9
+Console.WriteLine(numberA);
+int numberB = new Random ().Next(1, 10);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
