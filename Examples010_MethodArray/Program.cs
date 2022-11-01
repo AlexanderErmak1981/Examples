0b37497df0f4e2c4d85995fffd1c6ccfe2febafc
Alexander <ermakovich@km.ru>
2022-11-01 15:52:05 +0700

    Hello MethodArray!

---
+++ Examples010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 11, 21, 31, 41, 15, 18, 41, 19 };
+
+int n = array.Length;
+int find = 41;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+
+    index++;
+}
